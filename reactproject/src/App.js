0bcb54be047184d772267a6fed1a983f6eefed89
Hareshgvy <sabagvy07@gmail.com>
2024-08-22 20:15:06 -0300

    project
    project

---
+++ reactproject/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import WelcomeComponent from './Welcomecomponent';
+import { NewComponent,NewComponent2 } from './NewComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+               <WelcomeComponent/>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactproject/src/Welcomecomponent.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+export default function WelcomeComponent(){
+   var StudentName="Haresh"
+
+   var BatchName = "Tcs React"
+
+   var TeacherName = "Haresh"
+    return(
+       <div>
+          Trainer{TeacherName}is Teaching {StudentName}from {BatchName}
+       </div>
+    )
+}
